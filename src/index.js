9d225c497556599e7c3e2276e9cc64ebbca20eaa
RasmusIsraelsson <rasse980303@gmail.com>
2021-12-07 11:15:41 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-import IsLoading from './components/IsLoading/IsLoading.js';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './reduxStore/configureStore';
@@ -14,9 +13,7 @@ ReactDOM.render(
 	<Provider store={store}>
 		<React.StrictMode>
 			<ThemeProvider theme={mainTheme}>
-				<Suspense fallback={<IsLoading />}>
-					<App />
-				</Suspense>
+				<App />
 			</ThemeProvider>
 		</React.StrictMode>
 	</Provider>,
